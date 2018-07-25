6234f422b3d12a72500ea9983ee4ce42067d0d0c
Pratyush Poddar <wrick17@gmail.com>
2018-07-25 19:59:26 +0530

    stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,18 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pogo",
+        short_name: "Pogo",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "static/assets/icon.png", // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-sass',
     {
