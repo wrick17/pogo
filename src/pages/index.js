943d65712ac0bcc4833c52303c07bef4fd58a96e
Pratyush Poddar <wrick17@gmail.com>
2018-07-25 22:50:26 +0530

    added list to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = (props) => {
+  const blogs = props.data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter).filter(node => node.category);
+  const categoryMap = {};
+  blogs.forEach(blog => {
+    if (categoryMap[blog.category]) {
+      categoryMap[blog.category].push(blog);
+    } else {
+      categoryMap[blog.category] = [blog];
+    }
+  });
+  const categories = Object.keys(categoryMap);
   return (
     <div>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/blog/hello/">Go to Hello</Link>
+      {
+        categories.map(category => (
+          <div key={category}>
+            <h3>{category}</h3>
+            <ul>{
+              categoryMap[category].map(blog => (
+                <li key={blog.path}><Link to={blog.path}>{blog.title}</Link></li>
+              ))
+            }</ul>
+          </div>
+        ))
+      }
     </div>
   )
 }
